d4a0a1364ebbb32cfe144d8a3c7e406f0609958d
Lukas Šupienis <lukas.supienis@gmail.com>
2016-08-16 02:13:12 +0300

    Initial

---
+++ README.rst
@@ -0,0 +1,21 @@
+keyutil: SECRET_KEY generator taken from the Django project
+===========================================================
+
+keyutil is a python package which provides a sample Python CLI application with
+proper logging.
+
+.. _installation:
+
+Installation
+------------
+
+Supported versions of Python are: 2.7, 3.3, 3.4, and 3.5. The recommended way
+to install this package is via `pip <https://pypi.python.org/pypi/pip>`_.
+
+.. code-block:: bash
+
+    pip install keyutil
+
+For instructions on installing python and pip see "The Hitchhiker's Guide to
+Python" `Installation Guides
+<http://docs.python-guide.org/en/latest/starting/installation/>`_.
---
+++ keyutil/keyutil.py
@@ -0,0 +1,64 @@
+"""
+Random SECRET_KEY generator taken from the Django project
+"""
+from __future__ import unicode_literals
+
+import hashlib
+import random
+import time
+
+from os import getenv
+from six.moves import range
+
+
+# Use the system PRNG if possible
+try:
+    random = random.SystemRandom()
+    using_sysrandom = True
+except NotImplementedError:
+    import warnings
+    warnings.warn('A secure pseudo-random number generator is not available '
+                  'on your system. Falling back to Mersenne Twister.')
+    using_sysrandom = False
+
+
+def get_random_string(length=12,
+                      allowed_chars='abcdefghijklmnopqrstuvwxyz'
+                                    'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'):
+    """
+    Returns a securely generated random string.
+
+    The default length of 12 with the a-z, A-Z, 0-9 character set returns
+    a 71-bit value. log_2((26+26+10)^12) =~ 71 bits
+    """
+    if not using_sysrandom:
+        # This is ugly, and a hack, but it makes things better than
+        # the alternative of predictability. This re-seeds the PRNG
+        # using a value that is hard for an attacker to predict, every
+        # time a random string is required. This may change the
+        # properties of the chosen random sequence slightly, but this
+        # is better than absolute predictability.
+        random.seed(
+            hashlib.sha256(
+                ("%s%s%s" % (
+                    random.getstate(),
+                    time.time(),
+                    getenv('SECRET_KEY'))).encode('utf-8')
+            ).digest())
+    return ''.join(random.choice(allowed_chars) for i in range(length))
+
+
+def get_random_secret_key():
+    """
+    Return a 50 character random string usable as a SECRET_KEY setting value.
+    """
+    chars = 'abcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*(-_=+)'
+    return get_random_string(50, chars)
+
+
+def main():
+    print("SECRET_KEY = '{0}'".format(get_random_secret_key()))
+
+
+if __name__ == "__main__":
+    main()
