7ac099d39a76fdb9924d86edcfc6f6956fe52837
Lukas Šupienis <lukas.supienis@gmail.com>
2016-12-07 20:34:21 +0200

    Added console entry points

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-"""keyutil setup.py"""
+"""keyutil package installation script"""
 
 from setuptools import setup, find_packages
 
@@ -7,38 +7,46 @@ def readme():
     with open("README.rst") as f:
         return f.read()
 
-setup(name="keyutil",
-      author="Lukas Šupienis",
-      author_email="lukas.supienis@gmail.com",
-      classifiers=[
-          "Development Status :: 5 - Production/Stable",
-          "Intended Audience :: Developers",
-          "License :: OSI Approved :: GNU General Public License (GPL)",
-          "Natural Language :: English",
-          "Operating System :: OS Independent",
-          "License :: OSI Approved :: MIT License",
-          "Programming Language :: Python",
-          "Programming Language :: Python :: 2.7",
-          "Programming Language :: Python :: 3",
-          "Programming Language :: Python :: 3.3",
-          "Programming Language :: Python :: 3.4",
-          "Programming Language :: Python :: 3.5",
-          "Programming Language :: Python :: Implementation :: CPython",
-          "Topic :: Utilities",
-      ],
-      description=("utility to generate SECRET_KEYs for securing "
-                   "session data"),
-      install_requires=[
-          "docutils >=0.10",
-          ],
-      depends=[
-          "six ==1.10",
-          ],
-      keywords="python cli keyutil secret django flask",
-      license="BSD",
-      long_description=readme(),
-      packages=find_packages(exclude=["tests"]),
-      test_suite="tests",
-      url="http://github.com/lukassup/keyutil",
-      version="0.1.0",
-      zip_safe=True)
+
+setup(
+    name="keyutil",
+    author="Lukas Šupienis",
+    author_email="lukas.supienis@gmail.com",
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: GNU General Public License (GPL)",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Topic :: Utilities",
+    ],
+    description=("utility to generate SECRET_KEYs for securing "
+                 "session data"),
+    install_requires=[
+        "docutils >=0.10",
+    ],
+    depends=[
+        "six ==1.10",
+    ],
+    keywords="python cli keyutil secret django flask",
+    license="BSD",
+    long_description=readme(),
+    packages=find_packages(exclude=["tests"]),
+    test_suite="tests",
+    url="http://github.com/lukassup/keyutil",
+    version="0.1.0",
+    zip_safe=True,
+    entry_points={
+        'console_scripts': [
+            'keyutil=keyutil.keyutil:main',
+        ],
+    },
+)
